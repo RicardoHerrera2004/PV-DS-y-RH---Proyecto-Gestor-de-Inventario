ec72c252f0b2eb5ea5ea4e974a32f9198c73926b
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-04-21 20:55:15 -0500

    Agregar archivos de proyecto.

---
+++ PV, DS y RH - Proyecto Gestor de Inventario/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models;
+
+namespace PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PV, DS y RH - Proyecto Gestor de Inventario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
