b3643cbd8fbe8b3599be882f8f3c3da226674ee0
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-04-29 23:30:55 -0500

    Migracion a la base de datos e intento de corregir subir los datos a la base

--- PV, DS y RH - Proyecto Gestor de Inventario/Controllers/PedidoController.cs
+++ PV, DS y RH - Proyecto Gestor de Inventario/Controllers/PedidoController.cs
@@ -1,163 +1,179 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models;
 
-namespace PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Controllers
+public class PedidoController : Controller
 {
-    public class PedidoController : Controller
+    private readonly SQLInventarioPruebaContext _context;
+
+    public PedidoController(SQLInventarioPruebaContext context)
     {
-        private readonly SQLInventarioPruebaContext _context;
+        _context = context;
+    }
 
-        public PedidoController(SQLInventarioPruebaContext context)
+    // GET: Pedido
+    public async Task<IActionResult> Index()
+    {
+        var sQLInventarioPruebaContext = _context.Pedido.Include(p => p.Cliente);
+        return View(await sQLInventarioPruebaContext.ToListAsync());
+    }
+
+    // GET: Pedido/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: Pedido
-        public async Task<IActionResult> Index()
+        var pedido = await _context.Pedido
+            .Include(p => p.Cliente)
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (pedido == null)
         {
-            var sQLInventarioPruebaContext = _context.Pedido.Include(p => p.Cliente);
-            return View(await sQLInventarioPruebaContext.ToListAsync());
+            return NotFound();
         }
 
-        // GET: Pedido/Details/5
-        public async Task<IActionResult> Details(int? id)
+        return View(pedido);
+    }
+
+    // GET: Pedido/Create
+    public IActionResult Create()
+    {
+        ViewData["ClienteId"] = _context.Cliente
+            .Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Nombre + " " + c.Apellido
+            }).ToList();
+
+        return View();
+    }
+
+    // POST: Pedido/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,FechaPedido,Total,Estado,ClienteId")] Pedido pedido)
+    {
+        if (ModelState.IsValid)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var pedido = await _context.Pedido
-                .Include(p => p.Cliente)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (pedido == null)
-            {
-                return NotFound();
-            }
-
-            return View(pedido);
-        }
-
-        // GET: Pedido/Create
-        public IActionResult Create()
-        {
-            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Apellido");
-            return View();
-        }
-
-        // POST: Pedido/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FechaPedido,Total,Estado,ClienteId")] Pedido pedido)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(pedido);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Apellido", pedido.ClienteId);
-            return View(pedido);
-        }
-
-        // GET: Pedido/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var pedido = await _context.Pedido.FindAsync(id);
-            if (pedido == null)
-            {
-                return NotFound();
-            }
-            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Apellido", pedido.ClienteId);
-            return View(pedido);
-        }
-
-        // POST: Pedido/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FechaPedido,Total,Estado,ClienteId")] Pedido pedido)
-        {
-            if (id != pedido.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(pedido);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PedidoExists(pedido.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Apellido", pedido.ClienteId);
-            return View(pedido);
-        }
-
-        // GET: Pedido/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var pedido = await _context.Pedido
-                .Include(p => p.Cliente)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (pedido == null)
-            {
-                return NotFound();
-            }
-
-            return View(pedido);
-        }
-
-        // POST: Pedido/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var pedido = await _context.Pedido.FindAsync(id);
-            if (pedido != null)
-            {
-                _context.Pedido.Remove(pedido);
-            }
-
+            _context.Add(pedido);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool PedidoExists(int id)
+        ViewData["ClienteId"] = _context.Cliente
+            .Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Nombre + " " + c.Apellido
+            }).ToList();
+
+        return View(pedido);
+    }
+
+    // GET: Pedido/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
         {
-            return _context.Pedido.Any(e => e.Id == id);
+            return NotFound();
         }
+
+        var pedido = await _context.Pedido.FindAsync(id);
+        if (pedido == null)
+        {
+            return NotFound();
+        }
+
+        ViewData["ClienteId"] = _context.Cliente
+            .Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Nombre + " " + c.Apellido
+            }).ToList();
+
+        return View(pedido);
+    }
+
+    // POST: Pedido/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,FechaPedido,Total,Estado,ClienteId")] Pedido pedido)
+    {
+        if (id != pedido.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(pedido);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PedidoExists(pedido.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        ViewData["ClienteId"] = _context.Cliente
+            .Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Nombre + " " + c.Apellido
+            }).ToList();
+
+        return View(pedido);
+    }
+
+    // GET: Pedido/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var pedido = await _context.Pedido
+            .Include(p => p.Cliente)
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (pedido == null)
+        {
+            return NotFound();
+        }
+
+        return View(pedido);
+    }
+
+    // POST: Pedido/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var pedido = await _context.Pedido.FindAsync(id);
+        if (pedido != null)
+        {
+            _context.Pedido.Remove(pedido);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool PedidoExists(int id)
+    {
+        return _context.Pedido.Any(e => e.Id == id);
     }
 }
--- PV, DS y RH - Proyecto Gestor de Inventario/Data/SQLInventarioPruebaContext.cs
+++ PV, DS y RH - Proyecto Gestor de Inventario/Data/SQLInventarioPruebaContext.cs
@@ -5,20 +5,35 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models;
 
-    public class SQLInventarioPruebaContext : DbContext
+public class SQLInventarioPruebaContext : DbContext
+{
+    public SQLInventarioPruebaContext(DbContextOptions<SQLInventarioPruebaContext> options)
+        : base(options)
     {
-        public SQLInventarioPruebaContext (DbContextOptions<SQLInventarioPruebaContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models.Categoria> Categoria { get; set; } = default!;
-
-public DbSet<PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models.Producto> Producto { get; set; } = default!;
-
-public DbSet<PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models.Cliente> Cliente { get; set; } = default!;
-
-public DbSet<PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models.DetallePedido> DetallePedido { get; set; } = default!;
-
-public DbSet<PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models.Pedido> Pedido { get; set; } = default!;
     }
+
+    public DbSet<PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models.Categoria> Categoria { get; set; } = default!;
+
+    public DbSet<PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models.Producto> Producto { get; set; } = default!;
+
+    public DbSet<PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models.Cliente> Cliente { get; set; } = default!;
+
+    public DbSet<PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models.DetallePedido> DetallePedido { get; set; } = default!;
+
+    public DbSet<PV__DS_y_RH___Proyecto_Gestor_de_Inventario.Models.Pedido> Pedido { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Categoria>().HasData(
+            new Categoria { Id = 1, Nombre = "Ropa", Descripcion = "Prendas de vestir: camisetas, pantalones, chaquetas…" },
+            new Categoria { Id = 2, Nombre = "Comida", Descripcion = "Alimentos en conserva, envasados y frescos." },
+            new Categoria { Id = 3, Nombre = "Bebidas", Descripcion = "Bebidas alcohólicas y no alcohólicas." },
+            new Categoria { Id = 4, Nombre = "Abarrotes", Descripcion = "Productos de despensa: cereales, lácteos, granos." },
+            new Categoria { Id = 5, Nombre = "Joyas", Descripcion = "Collares, anillos, pulseras y accesorios metálicos." },
+            new Categoria { Id = 6, Nombre = "Fármacos", Descripcion = "Medicamentos y productos de cuidado de la salud." },
+            new Categoria { Id = 7, Nombre = "Otros", Descripcion = "Artículos diversos que no encajan en las categorías anteriores." }
+        );
+    }
+}
